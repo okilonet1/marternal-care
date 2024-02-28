c59626a8e150da9224041966da7babb2d67acb5e
Onyekachi James <okilonet1@gmail.com>
2024-02-28 03:39:25 +0100

    added small ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { SparklesCore } from "@/components/ui/sparkles";
 import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -12,5 +13,20 @@ export default async function AuthLayout({
     return redirect("/dashboard");
   }
 
-  return <>{children}</>;
+  return (
+    <div className="h-[100dvh] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden ">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+      <div className="z-10">{children}</div>
+    </div>
+  );
 }
--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/moving-border";
+import { Button as OrdButton } from "@/components/ui/button";
+
+import Link from "next/link";
 
 export default function Home() {
-  return <div>welcome</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h4>Welcome to </h4>
+      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+        MaternaAI
+      </h1>
+      <div className="flex flex-col lg:flex-row items-center justify-center mt-4 gap-4">
+        <Link href="/sign-in">
+          <OrdButton className="text-white rounded-[1.75rem] h-[62px] w-[158px] border-slate-800 bg-sky-500/85 hover:bg-transparent hover:border-sky-500/85">
+            Sign In
+          </OrdButton>
+        </Link>
+        <Link href="/sign-up">
+          <Button className="bg-transparent text-white dark:border-slate-800 hover:bg-sky-500 transition-all duration-200">
+            Sign Up
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
