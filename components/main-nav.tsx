014603cd568b7e5c03020e10408d3172a177e5aa
Onyekachi James <okilonet1@gmail.com>
2024-03-10 17:15:45 +0100

    update

---
+++ components/main-nav.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { validateRequest } from "@/lib/auth";
+
+export async function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const { user } = await validateRequest();
+
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
+      <h2 className="text-3xl font-bold tracking-tight">MaternaAI</h2>
+
+      <Link
+        href="/examples/dashboard"
+        className="text-sm font-medium transition-colors hover:text-primary"
+      >
+        Overview
+      </Link>
+      {user?.role === "ADMIN" && (
+        <>
+          <Link
+            href="/admin/users"
+            className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+          >
+            Users
+          </Link>
+        </>
+      )}
+      <Link
+        href="/examples/dashboard"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Settings
+      </Link>
+    </nav>
+  );
+}
---
+++ components/user-nav.tsx
@@ -0,0 +1,66 @@
+import { signOut } from "@/actions/auth.actions";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { validateRequest } from "@/lib/auth";
+
+export async function UserNav() {
+  const { user } = await validateRequest();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/avatars/01.png" alt="@maternaAI" />
+            <AvatarFallback>{`${user?.firstName[0]}${user?.lastName[0]}`}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {user?.firstName} {user?.lastName}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <form action={signOut}>
+            <Button
+              type="submit"
+              variant={"ghost"}
+              className="w-full items-center justify-center p-0 text-inherit"
+            >
+              Log out
+            </Button>
+          </form>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
